4ed49672f86b872f2bb91c149220f88b6beb7e1f
AmbreMandar1812 <ambremandar1812@gmail.com>
2024-05-14 14:28:36 +0530

    added getURLS from HTML and tests

--- crawl.js
+++ crawl.js
@@ -1,15 +1,46 @@
 //Three Process to write test 
 // 1. Stub out the function to test
-function normalizeURL(urlString){
-    const urlObj = new URL(urlString)
-    const hostPath = `${urlObj.hostname}${urlObj.pathname}`
-    if(hostPath.length > 0 && hostPath.slice(-1) === '/'){
-        return hostPath.slice(0,-1)
-    }
-    return `${urlObj.hostname}${urlObj.pathname}`
-}
 
-module.exports = {normalizeURL}
 // 2. Write the test for the function
 
 // 3. Go back and implement the meat of the function
+
+const {JSDOM} = require('jsdom')
+
+function getURLSfromHTML(inputBody,baseURL){
+    const urls = []
+    const dom = new JSDOM(inputBody)
+    const urlElements = dom.window.document.querySelectorAll("a");
+    // urlElements.push()
+    for(const urlElement of urlElements){
+        if(urlElement.href.slice(0,1) === '/'){
+            try{
+                const urlObj = new URL(`${baseURL}${urlElement.href}`)
+                urls.push(urlObj.href)
+            }catch(err){
+                console.log(`Error in the relative getURLSfromHTML ${err.message}`)
+            }
+        }else{
+            try {
+              const urlObj = new URL(`${urlElement.href}`);
+              urls.push(urlObj.href);
+            } catch (err) {
+              console.log(
+                `Error in the absolute getURLSfromHTML ${err.message}`
+              );
+            }
+        }
+    }
+    return urls
+}
+
+function normalizeURL(urlString) {
+  const urlObj = new URL(urlString);
+  const hostPath = `${urlObj.hostname}${urlObj.pathname}`;
+  if (hostPath.length > 0 && hostPath.slice(-1) === "/") {
+    return hostPath.slice(0, -1);
+  }
+  return `${urlObj.hostname}${urlObj.pathname}`;
+}
+
+module.exports = { normalizeURL,getURLSfromHTML };
--- crawl.test.js
+++ crawl.test.js
@@ -1,4 +1,4 @@
-const {normalizeURL} = require('./crawl.js')
+const {normalizeURL,getURLSfromHTML} = require('./crawl.js')
 const {test,expect} = require('@jest/globals')
 
 test('normalizeURL strip protocol',()=>{
@@ -19,5 +19,76 @@ test("normalizeURL strip capitals", () => {
   const input = "https://BLOG.boot.dev/path/";
   const actual = normalizeURL(input);
   const expected = "blog.boot.dev/path";
+  expect(actual).toEqual(expected);
+});
+ 
+
+test("getURLS from HTML absolute", () => {
+  const inputHTMLBody = `
+  <html>
+   <body> 
+    <a href = "https://blog.boot.dev/path">
+      Blog Boot DEV
+    </a>
+   </body>
+  </html>
+  `
+  const inputBaseURL = "https://blog.boot.dev"
+  const actual = getURLSfromHTML(inputHTMLBody,inputBaseURL);
+  const expected = ["https://blog.boot.dev/path"];
+  expect(actual).toEqual(expected);
+});
+
+test("getURLS from HTML relative", () => {
+  const inputHTMLBody = `
+  <html>
+   <body> 
+    <a href = "/path/">
+      Blog Boot DEV
+    </a>
+   </body>
+  </html>
+  `;
+  const inputBaseURL = "https://blog.boot.dev";
+  const actual = getURLSfromHTML(inputHTMLBody, inputBaseURL);
+  const expected = ["https://blog.boot.dev/path/"];
+  expect(actual).toEqual(expected);
+});
+
+test("getURLS from HTML both ", () => {
+  const inputHTMLBody = `
+  <html>
+   <body> 
+    <a href = "https://blog.boot.dev/path1/">
+      Blog Boot DEV 1
+    </a>
+    <a href = "/path2/">
+      Blog Boot DEV 2
+    </a>
+   </body>
+  </html>
+  `;
+  const inputBaseURL = "https://blog.boot.dev";
+  const actual = getURLSfromHTML(inputHTMLBody, inputBaseURL);
+  const expected = [
+    "https://blog.boot.dev/path1/",
+    "https://blog.boot.dev/path2/",
+  ];
+  expect(actual).toEqual(expected);
+});
+
+test("getURLS from HTML invalid", () => {
+  const inputHTMLBody = `
+  <html>
+   <body> 
+    <a href = "invalid">
+      Blog Boot DEV
+    </a>
+   </body>
+  </html>
+  `;
+  const inputBaseURL = "https://blog.boot.dev";
+  const actual = getURLSfromHTML(inputHTMLBody, inputBaseURL);
+  const expected = [];
   expect(actual).toEqual(expected);
 });
